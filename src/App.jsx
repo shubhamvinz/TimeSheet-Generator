bb9abad309040a1a730433dd7cd8a025e1b6ddab
animeshVG <animesh.raikwar@vinzglobal.com>
2024-11-28 19:16:49 +0530

    Ui Complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from './Components/Main'
+import "./App.css"
+const App = () => {
+  return (
+    <div className='Layout'>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Main.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import TextField from "@mui/material/TextField";
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import Button from "@mui/material/Button";
+
+const Main = () => {
+  return (
+    <div className="HeroSec">
+      {/* Logos Above the Box */}
+      <div className="logo">
+        <img src="./VINZ.png" alt="VINZ Logo" />
+        <img src="./Zoho.png" alt="Zoho Logo" />
+      </div>
+
+      {/* Form Section */}
+      <div className="Card">
+        <div className="fields">
+          {/* Row 1: List of Projects and List of Contributors */}
+          <div className="row">
+            <div className="input-group">
+              <label>
+                List of Projects <span style={{ color: "red" }}>*</span>
+              </label>
+              <FormControl fullWidth>
+                <InputLabel>Options</InputLabel>
+                <Select value="" onChange={() => {}}>
+                  <MenuItem value={10}>Option 1</MenuItem>
+                  <MenuItem value={20}>hello</MenuItem>
+                  <MenuItem value={30}>Option 3</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className="input-group">
+              <label>
+                List of Contributors <span style={{ color: "red" }}>*</span>
+              </label>
+              <FormControl fullWidth>
+                <InputLabel>Options</InputLabel>
+                <Select value="" onChange={() => {}}>
+                  <MenuItem value={10}>Option 1</MenuItem>
+                  <MenuItem value={20}>Option 2</MenuItem>
+                  <MenuItem value={30}>Option 3</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+
+          {/* Row 2: From and To */}
+          <div className="row">
+            <div className="input-group">
+              <label>
+                From <span style={{ color: "red" }}>*</span>
+              </label>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DesktopDatePicker
+                  label="Pick a Date"
+                  value={null}
+                  onChange={() => {}}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </div>
+            <div className="input-group">
+              <label>
+                To <span style={{ color: "red" }}>*</span>
+              </label>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DesktopDatePicker
+                  label="Pick a Date"
+                  value={null}
+                  onChange={() => {}}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="btn">
+          <Button variant="contained" className="generate-btn">
+            Generate
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
